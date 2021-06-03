9e488d2013a72ad5b250952f6a90ba4ad11be344
stephan du Plooy <sdpphotography1@gmail.com>
2021-06-02 20:38:26 -0400

    Changed Routes

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const homeRoutes = require('./customer-routes.js');
 const businessOwnerRoutes = require('./business-routes.js');
 const productRoutes = require('./product-routes');
 const cartRoutes = require('./cart-routes');
-const dashboardRoutes = require('./business-stats-routes');
+
 
 
 //router.use('/', homeRoutes);
@@ -13,8 +13,10 @@ router.use('/', productRoutes);
 router.use('/api', apiRoutes);
 router.use('/owner',businessOwnerRoutes)
 router.use('/cart', cartRoutes);
-router.use('/', dashboardRoutes);
+
 //router.use('/products', productRoutes);
 //router.use('/login', homeRoutes);
 
 module.exports = router;
+////add Date to profit table
+///make a new table with product_id, id, profit_id, price, quantity, date.
